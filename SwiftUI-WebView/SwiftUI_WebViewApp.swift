f34284a2f29cd0ed0900da0fab66e75e7448908a
Vamsi Kallepalli <vkallepalli@uworld.com>
2024-10-15 14:37:57 -0500

    Initial Commit

---
+++ SwiftUI-WebView/SwiftUI_WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WebViewApp.swift
+//  SwiftUI-WebView
+//
+//  Created by Vamsi Kallepalli on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
