e83c1edb49d74018bccd111b2934f1f4bb03f9ae
Vamsi Kallepalli <vkallepalli@uworld.com>
2024-10-29 11:38:02 -0500

    first commit

---
+++ SwiftUI-WebView/SwiftUIView.swift
@@ -0,0 +1,18 @@
+//
+//  SwiftUIView.swift
+//  SwiftUI-WebView
+//
+//  Created by Vamsi Kallepalli on 10/15/24.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SwiftUIView()
+}
---
+++ SwiftUI-WebView/WebViewContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewContainerView.swift
+//  SwiftUI-WebView
+//
+//  Created by Vamsi Kallepalli on 10/22/24.
+//
+
+import SwiftUI
+
+struct WebViewContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WebViewContainerView()
+}
